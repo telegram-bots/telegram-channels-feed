a386ebce934a3c3639ca89d06cb351ae4933a1c4
REDNBLACK <rednblack@protonmail.com>
2017-07-22 16:45:36 +0300

    Fix for issue#42

--- bot/src/tg_cli.py
+++ bot/src/tg_cli.py
@@ -15,17 +15,14 @@ class TelegramCLI:
     def __init__(self, config):
         self.config = config
         self.user_id = config.getint('id')
-        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.retries = 0
-        self.connected = False
-        self.__connect()
+        self.socket = None
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         self.__disconnect()
 
     def lookup_channel(self, channel_url: str) -> Tuple[int, str]:
         command = '{"ID": "SearchPublicChat", "username_": "%s"}'
-        response = self.__send_and_receive(command % channel_url)
+        response = self.__send(command % channel_url)
         json_data = json.loads(response[0], encoding=encoding)
         channel_id = json_data['id_']
         channel_name = json_data['title_']
@@ -34,7 +31,7 @@ class TelegramCLI:
 
     def subscribe_to_channel(self, id_: int):
         command = '{"ID": "AddChatMember", "chat_id_": %d, "user_id_": %d, "forward_limit_": 2}'
-        response = self.__send_and_receive(command % (id_, self.user_id))
+        response = self.__send(command % (id_, self.user_id))
         json_data = json.loads(response[0], encoding=encoding)
 
         if json_data['ID'] != 'Ok':
@@ -42,40 +39,52 @@ class TelegramCLI:
 
     def unsubscribe_from_channel(self, id_: int):
         command = '{"ID": "ChangeChatMemberStatus", "chat_id_": %d, "user_id_": %d, "status_": {"ID": "ChatMemberStatusLeft"}}'
-        response = self.__send_and_receive(command % (id_, self.user_id))
+        response = self.__send(command % (id_, self.user_id))
         json_data = json.loads(response[0], encoding=encoding)
 
         if json_data['ID'] != 'Ok':
             raise ConnectionError(f"[tg-cli] Failed to unsubscribe: {json_data}")
 
-    def __connect(self):
-        if self.retries > 5:
+    def __connect(self, retries=5):
+        if retries == 0:
             logging.error('[tg-cli] Maximum number of retries reached. Stopping')
             return
 
         try:
-            logging.info(f"[tg-cli] Connecting to {self.config['host']}:{self.config['port']}")
+            self.__disconnect()
+            logging.debug(f"[tg-cli] Connecting to {self.config['host']}:{self.config['port']}")
+            self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             self.socket.connect((self.config['host'], self.config.getint('port')))
-            self.connected = True
-            logging.info('[tg-cli] Connected')
+            logging.debug('[tg-cli] Connected')
         except socket.error as e:
+            self.__disconnect()
             logging.warning(f"[tg-cli] Failed to connect : {str(e)}")
             time.sleep(5)
-            self.__connect()
+            self.__connect(retries - 1)
+        except Exception as e:
+            self.__disconnect()
+            raise e
 
     def __disconnect(self):
-        if self.connected:
+        if self.socket is not None:
             self.socket.close()
-            self.connected = False
+            self.socket = None
 
-    def __send(self, command):
-        if not self.connected:
-            raise ConnectionError('[tg-cli] Not connected to tg-cli!')
-        if self.EOL in command:
-            command = str.join(' ', command.strip(self.EOL))
-        self.socket.sendall((command + self.EOL).encode(encoding))
-        time.sleep(0.5)
+    def __send(self, command) -> List[str]:
+        try:
+            self.__connect()
 
-    def __send_and_receive(self, command) -> List[str]:
-        self.__send(command)
-        return self.socket.recv(self.BUFFER_SIZE).decode(encoding).strip().split(self.EOL)[1:]
+            if self.EOL in command:
+                command = str.join(' ', command.strip(self.EOL))
+            command = (command + self.EOL).encode(encoding)
+
+            self.socket.sendall(command)
+            time.sleep(0.5)
+            return self.socket.recv(self.BUFFER_SIZE) \
+                       .decode(encoding) \
+                       .strip() \
+                       .split(self.EOL)[1:]
+        except socket.error:
+            self.__send(command)
+        finally:
+            self.__disconnect()
