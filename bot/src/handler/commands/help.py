0cf20cb860aa26d1090c8fea08374ef7349e6a51
REDNBLACK <rednblack@protonmail.com>
2017-07-25 02:27:08 +0300

    Enabled markdown formatting for help command

--- bot/src/handler/commands/help.py
+++ bot/src/handler/commands/help.py
@@ -1,12 +1,13 @@
 import os
 
+from telegram.parsemode import ParseMode
 from src.utils import read_to_string
 from . import Base
 
 
 class Help(Base):
     name = 'help'
-    text = read_to_string(os.path.join('resources', 'info', 'help.txt'))
+    text = read_to_string(os.path.join('resources', 'info', 'help.md'))
 
     def execute(self, command):
-        self.reply(command, self.text)
+        self.reply(command, self.text, parse_mode=ParseMode.MARKDOWN)
